ab7533aa791f6db6cb7313c7bb751f23f39c19ab
rossa <fashionfish@naver.com>
2021-10-27 12:57:18 +0900

    Work pages

--- assets/js/sticky.js
+++ assets/js/sticky.js
@@ -4,15 +4,17 @@
 
     // sticky 효과
     document.querySelectorAll('[data-sticky]').forEach((stickyElem) => {
+      const sectionElem = stickyElem.parentElement;
       const revealElem = stickyElem.querySelector('[data-reveal]');
       const outlinedElem = stickyElem.querySelector('[data-outlined]');
 
       const offsetTop = stickyElem.offsetTop;
+      const sectionHeight = sectionElem.clientHeight;
       const outlinedHeight = outlinedElem.clientHeight;
 
       const cha = scrollTop - offsetTop;
 
-      if (scrollTop >= offsetTop && cha <= outlinedHeight) {
+      if (scrollTop >= offsetTop && cha <= sectionHeight / 2 - 30) {
         stickyElem.style.transform = `translate(0px, ${cha}px)`;
         revealElem.style.height = `${outlinedHeight - cha}px`;
       }
