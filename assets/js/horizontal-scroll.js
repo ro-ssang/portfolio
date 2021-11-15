d57dcf4aa359c936e5b3e3d4fc4dfb575afdbff4
rossa <fashionfish@naver.com>
2021-11-15 10:01:45 +0900

    chore. 가로 스크롤링 스크립트 수정

--- assets/js/horizontal-scroll.js
+++ assets/js/horizontal-scroll.js
@@ -1,22 +1,28 @@
 (function () {
-  const SITE_WIDTH_SCALE = 1.3;
-
   const section = document.querySelector('.sec4');
   const sitesCont = document.querySelector('.sec4 .sites_cont');
   const wrapper = document.querySelector('.sec4 .sites_wrapper');
   const sites = document.querySelectorAll('.sec4 .site');
 
-  const wHeight = window.innerHeight;
-
   let scrollWidth = 0;
-  sites.forEach((site) => (scrollWidth += site.clientWidth));
-  wrapper.style.width = sites[0].clientWidth * (1 / SITE_WIDTH_SCALE) + 'px';
 
-  sitesCont.style.height = scrollWidth + 'px';
+  function setSize() {
+    sites.forEach((site) => (scrollWidth += site.clientWidth));
+    const wrapperWidth = window.innerWidth < 1500 ? window.innerWidth : 1500;
+    wrapper.style.width = wrapperWidth + 'px';
 
-  window.window.addEventListener('scroll', () => {
+    sitesCont.style.height = scrollWidth + 'px';
+  }
+
+  setSize();
+  window.addEventListener('resize', () => {
+    scrollWidth = 0;
+    setSize();
+  });
+  window.addEventListener('scroll', () => {
     const scrollTop = window.scrollY;
     const offsetTop = section.offsetTop + sitesCont.offsetTop;
+    const wHeight = window.innerHeight;
 
     if (scrollTop >= offsetTop) {
       sitesCont.style.paddingTop = scrollWidth - wHeight + 'px';
@@ -44,24 +50,30 @@
 })();
 
 (function () {
-  const SITE_WIDTH_SCALE = 1.3;
-
   const section = document.querySelector('.sec6');
   const sitesCont = document.querySelector('.sec6 .sites_cont');
   const wrapper = document.querySelector('.sec6 .sites_wrapper');
   const sites = document.querySelectorAll('.sec6 .site');
 
-  const wHeight = window.innerHeight;
-
   let scrollWidth = 0;
-  sites.forEach((site) => (scrollWidth += site.clientWidth));
-  wrapper.style.width = sites[0].clientWidth * (1 / SITE_WIDTH_SCALE) + 'px';
 
-  sitesCont.style.height = scrollWidth + 'px';
+  function setSize() {
+    sites.forEach((site) => (scrollWidth += site.clientWidth));
+    const wrapperWidth = window.innerWidth < 1500 ? window.innerWidth : 1500;
+    wrapper.style.width = wrapperWidth + 'px';
 
-  window.window.addEventListener('scroll', () => {
+    sitesCont.style.height = scrollWidth + 'px';
+  }
+
+  setSize();
+  window.addEventListener('resize', () => {
+    scrollWidth = 0;
+    setSize();
+  });
+  window.addEventListener('scroll', () => {
     const scrollTop = window.scrollY;
     const offsetTop = section.offsetTop + sitesCont.offsetTop;
+    const wHeight = window.innerHeight;
 
     if (scrollTop >= offsetTop) {
       sitesCont.style.paddingTop = scrollWidth - wHeight + 'px';
